ed7ee9f0f273f83ed6625d8cd07b8c2ea6796b99
Wimme Myrskylä <wimme.myrskyla@metropolia.fi>
2026-09-18 02:40:13 +0300

    lol

--- peliprojekti/README.md
+++ peliprojekti/README.md
@@ -1,2 +1,6 @@
 Pelin nimi?
 Wimme Myrskylä
+
+
+Pelin ideana on matkustaa Helsingistä Nuorgamiin valitsemallaan ajoneuvolla. Matkanvarrella pelaaja valitsee reitin ja reitin eri kaupungeissa tapahtuu asioita. 
+
--- peliprojekti/peli.py
+++ peliprojekti/peli.py
@@ -1,22 +1,27 @@
 nimi = input("Hei! Kirjoita nimesi: \n")
 ikä = int(input("Kirjoita ikäsi: \n"))
 bool1 = True
+pelin_aloitus = False
 
 while True:
 
-    if ikä >= 12 and bool1 == True:
+    if ikä >= 12 and bool1 == True and pelin_aloitus == False:
         print(f"Kiitos {nimi}! \n")
         print("PÄÄVALIKKO\n")
 
+        print("\n 'Aloita', 'Lopeta', 'Tauko'")
+
         while bool1 == True:
             
-            komento = input("Syötä komento: \n")
+            komento = input("\nSyötä komento: \n")
 
             if komento == "Lopeta":
                 bool1 = False
                 break
             elif komento == "Aloita":
                 print("Aloitetaan peli")
+                pelin_aloitus = True
+                break
 
             elif komento == "Tauko":
                 print("Peli tauolla paina enter jatkaaksesi peliä")
@@ -28,11 +33,18 @@ while True:
             else:
                 print(f"kirjoitit komennon {komento}, josta ei tapahdu mitään. Jos haluat sulkea pelin, niin kirjoita komento 'Lopeta'")
 
+    if pelin_aloitus == True:
+        print("Aloitus onnistui")
+        print("Inventaario, Raha, ")
+        komento = input("Syötä komento:\n")
+
     elif ikä < 12:
         print("Olet valitettavasti alaikäinen")
         break
     else:
         break
+
+
 
 print("Kiitos pelaamisesta")
 
