069860ff10f8609b011ec2e82d3a832f34d734c9
Wimme Myrskylä <wimme.myrskyla@metropolia.fi>
2026-09-18 03:21:34 +0300

    lol

--- peliprojekti/peli.py
+++ peliprojekti/peli.py
@@ -3,6 +3,10 @@ ikä = int(input("Kirjoita ikäsi: \n"))
 bool1 = True
 pelin_aloitus = False
 
+raha = int(1)
+
+inventaario = []
+
 while True:
 
     if ikä >= 12 and bool1 == True and pelin_aloitus == False:
@@ -34,9 +38,46 @@ while True:
                 print(f"kirjoitit komennon {komento}, josta ei tapahdu mitään. Jos haluat sulkea pelin, niin kirjoita komento 'Lopeta'")
 
     if pelin_aloitus == True:
-        print("Aloitus onnistui")
-        print("Inventaario, Raha, ")
+
+        def inventaariolis_f(x):
+            if x == "":
+                return(inventaario)
+            else:
+                inventaario.append(x)
+                return(inventaario)
+        def inventaario_f():
+            for i in inventaario:
+                print(i)
+
+        def rahalis(x):
+
+            global raha
+            
+            if int(x) > 0:
+                print(f"{raha} + {x}")
+            elif int(x) < 0:
+                print(f"{raha} - {x}")
+            raha += int(x)
+            print(f"Rahaa tilillä {raha}")
+            return(raha)
+        
+
+
+        print("Inventaario, Lompakko, ")
         komento = input("Syötä komento:\n")
+
+        if komento == "Inventaario":
+            inventaario_f()
+
+        elif komento == "Lisää inventaarioon":
+            inventaario_lisäys = input("Kirjoita tähän: \n")
+            inventaariolis_f(inventaario_lisäys)
+
+        elif komento == "Rahaa":
+            raha1 = int(input("Syötä kelan tuet:\n")) 
+            rahalis(raha1)
+        
+        
 
     elif ikä < 12:
         print("Olet valitettavasti alaikäinen")
